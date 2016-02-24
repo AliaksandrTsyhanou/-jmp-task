2e51ba0bb3c3527c8c74018be241b57e8a085df0
Aliaksandr_Tsyhanou <Tsyganov.work@gmail.com>
2016-02-25 01:17:49 +0300

    realized copy base. Need refactoring.

--- task-9-SQL/src/main/java/SQLMain.java
+++ task-9-SQL/src/main/java/SQLMain.java
@@ -1,16 +1,18 @@
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SQLMain {
 
     private static String className = "com.mysql.jdbc.Driver";
-    private static String DB_URL_FROM = "jdbc:mysql://localhost/issuetracker";
-    private static String DB_URL_TO = "jdbc:mysql://localhost/copydb";
+    private static final String DB_URL_FROM = "jdbc:mysql://localhost/issuetracker";
+    private static final String DB_URL_TO = "jdbc:mysql://localhost/copydb";
     private static String user = "root";
     private static String password = "1234";
+
+    private static final String SQL_PARAM_DELIMETR = ", ";
 
     static{
         try {
@@ -22,21 +24,55 @@ public class SQLMain {
 
     public static void main(String[] args) {
 
-        try( Connection con = DriverManager.getConnection(DB_URL_FROM, user, password)){
-            Set<String> tableNames = getTableNames(con);
-            tableNames.forEach(n ->{
-                System.out.println("table:" + n);
-                try {
-                    getTableColumNames(con, n).forEach(c -> {System.out.println("   " + c);});
+        try( Connection conFrom = DriverManager.getConnection(DB_URL_FROM, user, password);
+             Connection conTo = DriverManager.getConnection(DB_URL_TO, user, password);){
+            Set<String> tableNames = getTableNames(conFrom);
+            tableNames.forEach(tableName ->{
+                System.out.println("table:" + tableName);
+                try (Statement stFrom = conFrom.createStatement();
+                     Statement stTo = conTo.createStatement()){
+                    getTableColumNames(conFrom, tableName).forEach(c -> {System.out.println("   " + c);});
+                    createTable(stFrom, stTo, tableName);
+                    insertDataToTable(stFrom, stTo, tableName);
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             });
-            List<String> listTableNames = new ArrayList<>(tableNames);
-            System.out.println(getCreateTableSql(con, listTableNames.get(1)));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void createTable(Statement stFrom, Statement stTo, String tableName) throws SQLException {
+        String createTableSql = getCreateTableSql(stFrom, tableName);
+        System.out.println(createTableSql);
+        stTo.execute("DROP TABLE IF EXISTS `" + tableName + "`");
+        stTo.execute(createTableSql);
+    }
+
+    private static void insertDataToTable( Statement stFrom, Statement stTo, String tableName) throws SQLException {
+        String startInsertSql = "INSERT INTO `" + tableName + "` VALUES (";
+        ResultSet rs = stFrom.executeQuery("SELECT * FROM " + tableName);
+        int columnCont =  rs.getMetaData().getColumnCount();
+        while(rs.next()){
+            StringJoiner sj = new StringJoiner(",");
+//                    for(int i = 1; i<=columnCont; i++){
+//                        sj.add(rs.getObject(i).toString());
+//                    }
+            IntStream.rangeClosed(1, columnCont).forEach((i) -> {
+                try {
+                    System.out.println("rs.getMetaData().getColumnType("+i+"):" + rs.getMetaData().getColumnType(i));
+                    String value = rs.getObject(i) == null ? "NULL" : "'" + rs.getObject(i).toString() + "'";
+                    sj.add(value);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            String insertSQL = (new StringJoiner("")).add(startInsertSql).add(sj.toString()).add(");").toString();
+            System.out.println("insertSQL:" + insertSQL);
+            stTo.addBatch(insertSQL);
+        }
+        stTo.executeBatch();
     }
 
     private static Set<String> getTableNames(Connection con) throws SQLException {
@@ -59,8 +95,8 @@ public class SQLMain {
         return columNames;
     }
 
-    private static String getCreateTableSql(Connection con, String tableName) throws SQLException {
-        DatabaseMetaData metaData = con.getMetaData();
+    private static String getCreateTableSql(Statement st, String tableName) throws SQLException {
+        DatabaseMetaData metaData = st.getConnection().getMetaData();
 
         Set<String> pkNames = new HashSet<String>();
 
@@ -76,7 +112,7 @@ public class SQLMain {
         String delim = "";
         while( columnsMetadata.next() ) {
             sql.append( delim );
-            delim = ", ";
+            delim = SQL_PARAM_DELIMETR;
 
             String columnName = columnsMetadata.getString( 4 );
             String sqlTypeName = columnsMetadata.getString(6);
@@ -100,7 +136,7 @@ public class SQLMain {
             precision = 0;
         }
         buffer.append( identifierQuoteString + columnName + identifierQuoteString + " " + targetTypename );
-        if( columnSize > 0 ) {
+        if( columnSize > 0 && !"DATETIME".equals(targetTypename)) {
             buffer.append( "(" + columnSize );
             if( precision > 0 ) {
                 buffer.append( "," + precision );
